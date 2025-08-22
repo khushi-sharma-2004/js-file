8a8c6f4045751eb41115a84b7e5d907357e452b3
khushi-sharma-2004 <khushisharma.29102004@gmail.com>
2025-08-22 18:34:44 +0000

    operation

--- 01_BASICS/02conversionop.js
+++ 01_BASICS/02conversionop.js
@@ -24,3 +24,41 @@ let stringNum=String(someNum)
 //console.log(stringNum);
 //console.log(typeof stringNum);
                                                                                                                                                                                                                                                                                                                                                                 
+// ******************************Operations*********************
+let value=3
+let negValue=-value
+//console.log(negValue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);*/
+let str1="hello"
+let str2="khushi"
+let str3= str1+" "+str2
+/*console.log(str3);
+console.log("1"+2);//op= 12
+console.log("1"+2+2);// o/p= 122 : due to preffered guidlines
+console.log(1+2+"2");// o/p = 32 :due to preferred guidelines*/
+//console.log((3+4)*5%3);//always use paranthesis
+  
+
+
+/***************************some special but not good practise conversion****************/
+//console.log(+true);
+//console.log(true+);
+//console.log(+"");
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
