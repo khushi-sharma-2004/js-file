8f993efb11a3e488a8f359c3ff1b1a25a5d55dd0
khushi-sharma-2004 <khushisharma.29102004@gmail.com>
2025-08-22 17:55:11 +0000

    conversion of  datatypes

---
+++ 01_BASICS/02conversionop.js
@@ -0,0 +1,26 @@
+//let score= null, //here the type is object on conersion into num it gives 0 as value
+//let score=undefined // here in undefined case it will give nan as value 
+/* let score="khushi"//gives nan after conversion
+//let score =true//gives 1 after conversion
+//let score=false//gives 0// after conversion
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNum=Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);//js is not that strict */
+
+
+//let isLoggedIn=1
+let isLoggedIn="KHUSHI"
+let booleanIsLoggedIn =Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn)
+/* EMPTY STRING -AFTER CONVERSION-FASLSE
+STRING -AFTER CONV - TRUE
+1-AFTER  CONV -TRUE
+0-FALSE
+*/
+let someNum=33
+let stringNum=String(someNum)
+//console.log(stringNum);
+//console.log(typeof stringNum);
+                                                                                                                                                                                                                                                                                                                                                                
