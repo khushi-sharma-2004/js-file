4112ea889f99c07ef5bb14035c9d18f601334638
khushi-sharma-2004 <khushisharma.29102004@gmail.com>
2025-08-23 13:50:54 +0000

    memorytypes

--- 01_BASICS/05memorytypes.js
+++ 01_BASICS/05memorytypes.js
@@ -1,3 +1,21 @@
 /*******************STACK//HEAP ***********************/
 //PRIMITIVE:STACK
 //NONPRIMITIVE:HEAP
+let num1=123
+let num2=num1
+num2=124
+console.log(num1);
+console.log(num2);///since it is a stack memory
+
+
+
+let myobj={
+    name: "Riya",
+    age:24,
+}
+let yourobj=myobj
+yourobj.age=25
+console.log(myobj);
+console.log(yourobj);///since it points toward heap memory
+
+
