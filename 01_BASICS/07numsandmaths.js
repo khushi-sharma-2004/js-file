b846dec615984b6a5cc413e0686b4b7988b6d31b
khushi-sharma-2004 <khushisharma.29102004@gmail.com>
2025-08-25 05:34:09 +0000

    NumsAndMaths

---
+++ 01_BASICS/07numsandmaths.js
@@ -0,0 +1,29 @@
+const score=300
+//console.log(score);
+const balance = new Number(100)
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(5));
+
+const n = 23.998998
+//console.log(n.toPrecision(3));
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-In'));
+//console.log(Math.min(n,hundreds));
+//console.log(Math.max(n,hundreds));
+
+
+
+/*****************Maths**************************/
+console.log(hundreds);
+
+console.log(Math.round(32.9876));
+console.log(Math.floor(87.32));
+console.log(Math.ceil(87.32));
+const max  =30
+const min=20
+
+console.log(Math.floor(Math.random()*(max-min+1)));
+
+
+
